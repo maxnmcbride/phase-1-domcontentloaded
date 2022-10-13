c1cc25175ed28831a28027c930011ae2bc517cf4
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-10-13 12:36:20 -0400

    commit tree 499c03124cef6c99abc51215d0bc4e88e5a4f59c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const thing = document.querySelector('#text')
+    thing.textContent = "This is really cool!"
+});
